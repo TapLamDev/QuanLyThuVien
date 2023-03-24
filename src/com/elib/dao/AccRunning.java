8889e0cd8a243ed8179df78573b7f7d08322a152
Admin <Admin@Admin>
2023-03-24 17:28:50 +0700

    dasdas

---
+++ src/com/elib/dao/AccRunning.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.elib.dao;
+
+/**
+ *
+ * @author Admin
+ */
+public class AccRunning {
+    private static String ChucVu, Ma, Ho, Ten, NgaySinh, CCCD, Sdt, Email;
+}
